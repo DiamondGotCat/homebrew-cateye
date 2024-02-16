6a67d7cd9f163e59b1372cc0ae1a430234f6ed44
TechCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-02-16 10:12:28 +0900

    Update cateye.rb

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -3,7 +3,7 @@ class cateye < Formula
   homepage "https://www.github.com/DiamondGotCat/cateye/"
   url "https://raw.githubusercontent.com/DiamondGotCat/cateye/master/Formula/cateye"
   sha256 "dcaceaa173e987b6f953616dbd84db6764669938764b16cf4114f321d0b98834"
-
+  depends_on "jq"
   # depends_on "dependency" => :optional
 
   def install
