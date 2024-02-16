a0dd1194eb3d942a5a5466f69553d7be89fd1bc1
TechCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-02-16 10:08:54 +0900

    Update cateye.rb

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -1,4 +1,4 @@
-class YourPackageName < Formula
+class cateye < Formula
   desc "Download tool from Kamu Dev and Install."
   homepage "https://www.github.com/DiamondGotCat/cateye/"
   url "https://raw.githubusercontent.com/DiamondGotCat/cateye/master/Formula/cateye"
