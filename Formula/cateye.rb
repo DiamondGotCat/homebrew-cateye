46e24ccd739093635159532dae02eccc97cc9aab
TechCat56 <124330624+DiamondGotCat@users.noreply.github.com>
2024-02-23 23:34:33 +0900

    Update cateye.rb

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -5,7 +5,7 @@ class Cateye < Formula
   url "https://dl.kamu.jp/cateye/cateye"
   sha256 "174841a90ac0f983cd4aaa8e2384bdf76087474e2f68a43620b9e44bca9f545b"
   depends_on "jq"
-  version "1.2"
+  version "1.3"
   # depends_on "dependency" => :optional
 
   def install
