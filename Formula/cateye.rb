674b068182b738516b912982e26da627bfe30647
TechCat56 <chii.2019.5555@gmail.com>
2024-03-04 18:43:20 +0900

    Update

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -2,9 +2,9 @@ class Cateye < Formula
   desc "Download and Install Open-source Software"
   homepage "https://www.github.com/DiamondGotCat/cateye/"
   url "https://dl.kamu.jp/cateye/cateye"
-  sha256 "90709f5b8b861efb598d66c678b0184be19e14d5a504f35e389b06a3e6fcd12c"
+  sha256 "a93635c0b746116dc7a12a432b86b5eb819e257119279b473310cf98323540bc"
   depends_on "jq"
-  version "7.1.2"
+  version "7.1.3"
 
   def install
     bin.install "cateye"
