0ecb3cdf18d8e7751154164ccfae8da1be4de01a
CatKun38 <124330624+DiamondGotCat@users.noreply.github.com>
2024-04-14 19:16:27 +0900

    Update cateye.rb

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -2,7 +2,7 @@ class Cateye < Formula
   desc "Download and Install Open-source Software"
   homepage "https://www.github.com/DiamondGotCat/cateye/"
   url "https://kamu.jp/files/cateye"
-  sha256 "689b3a83ba20b3188e65ae4a715c5ba5aa4f750c09a6ec403ce37d65051185c9"
+  sha256 "2463361d2a9c28d5467884f47c295f22f4b7b6c7bd0d66e6c86559a3fdd2ca7f"
   depends_on "jq"
   version "8.6.0"
 
