3546acc681815e4b2458dcf14c2b6d42e4b4278e
技術猫 <124330624+DiamondGotCat@users.noreply.github.com>
2024-04-22 15:46:08 +0900

    Update cateye.rb

--- Formula/cateye.rb
+++ Formula/cateye.rb
@@ -1,17 +1,16 @@
 class Cateye < Formula
-  desc "Download and Install Open-source Software"
-  homepage "https://github.com/DiamondGotCat/cateye/"
-  url "https://kamu.jp/files/cateye-brew"
-  sha256 "9c1efbf2ec2dc923bde6aab667f25a2810d7d086950e7dca600f7cbc6570fb24"
-  depends_on "jq"
-  version "8.6.4"
+  desc 'Download and Install Open-source Software'
+  homepage 'https://github.com/DiamondGotCat/cateye/'
+  url 'https://kamu.jp/files/cateye'
+  sha256 '9c1efbf2ec2dc923bde6aab667f25a2810d7d086950e7dca600f7cbc6570fb24'
+  depends_on 'jq'
+  version '8.6.4'
 
   def install
-    File.rename("cateye-brew","cateye")
-    bin.install "cateye"
+    bin.install 'cateye'
   end
 
   test do
-    # None
+    cateye doctor
   end
 end
